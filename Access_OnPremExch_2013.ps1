290df68abc71ed59af3aaea2f7ef640d643aedbd
itmondemand <42688594+itmondemand@users.noreply.github.com>
2018-08-29 18:12:07 +1000

    File uploaded

---
+++ Access_OnPremExch_2013.ps1
@@ -0,0 +1,8 @@
+﻿$credential = Get-Credential
+$MailServer = Read-Host -Prompt "State the name of the server hosting Exchange services"
+$Exchsession = New-PSSession -ConnectionUri http://$MailServer/powershell/ -ConfigurationName Microsoft.Exchange -Authentication Kerberos -Credential $credential
+Import-PSSession $Exchsession
+Import-Module ActiveDirectory
+if (!$Error) {
+Write-Host -ForegroundColor Green "You are now connected to the Exchange and ActiveDirectory sessions"
+}
